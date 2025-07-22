877c03dd511190b380c1ed112b0ce9b31301a545
Sandun Madhushan <sandunhmadushan9330@gmail.com>
2025-07-23 00:46:26 +0530

    Remove mock data and upload page
    Deleted src/data/mockData.ts and src/pages/UploadMockData.tsx to clean up unused mock data and related upload functionality.

--- src/pages/UploadMockData.tsx
+++
@@ -1,9 +0,0 @@
-import { useEffect } from "react";
-import { uploadAllMockData } from "../utils/uploadMockData";
-
-export default function UploadMockDataPage() {
-  useEffect(() => {
-    uploadAllMockData();
-  }, []);
-  return <div>Uploading mock data...</div>;
-}
